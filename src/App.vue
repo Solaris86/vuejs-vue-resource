fd0b2702365309e1f99e9ab56269a3841bf7ec05
Solaris86 <nikola.suscevic@gmail.com>
2019-01-18 15:45:34 +0100

    Final commit

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,10 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
+                <input type="text" class="form-control" v-model="node">
+                <br>
                 <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <br><br>
                 <ul class="list-group">
                     <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
                 </ul>
@@ -28,38 +31,61 @@
             return {
               user: {
                   username: '',
-                  email: ''
+                  email: '',
+                  node: 'data'
               },
               users: [],
-              resource: {}
+              resource: {},
+              node: 'data'
             };
         },
         methods: {
             submit() {
-                this.$http.post('data.json', this.user)
+                // this.$http.post('data.json', this.user)
                     // .then(response => {
                     //     console.log(response);
                     // }, error => {
                     //     console.log(error);
                     // });
-                this.resource.save({}, this.user);
+                // this.resource.save({}, this.user);
+                this.resource.saveAlt(this.user);
             },
             fetchData() {
-                this.$http.get('data.json')
-                    .then(response => {
-                        return response.json();
+                // this.$http.get('data.json')
+                //     .then(response => {
+                //         return response.json();
+                //     })
+                //     .then(data => {
+                //         const resultArray = [];
+                //         for (let key in data) {
+                //             resultArray.push(data[key]);
+                //         }
+                //         this.users = resultArray;
+                //     });
+                this.resource.getData({'node': this.node})
+                    .then((response) => {
+                       return response.json();
                     })
                     .then(data => {
-                        const resultArray = [];
-                        for (let key in data) {
-                            resultArray.push(data[key]);
-                        }
-                        this.users = resultArray;
+                       const resultArray = [];
+                       for (let key in data) {
+                           resultArray.push(data[key]);
+                       }
+                       this.users = resultArray;
                     });
             }
         },
         created() {
-            this.resource = this.$resource('data.json');
+            const customActions = {
+              saveAlt: {
+                  method: 'POST',
+                  url: 'alternative.json'
+              },
+              getData: {
+                  method: 'GET'
+              }
+            };
+            this.resource = this.$resource('{node}.json', {}, customActions);
         }
     }
 </script>
